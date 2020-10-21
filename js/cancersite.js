df7974afc89b8b44295e5b6c74595e9c56b3f554
unknown <beingstrong40@gmail.com>
2020-10-21 18:18:41 -0500

    Updated index, style.css and added several images, updated two of the javascript files, and added several other HTML pages to the project.

--- js/cancersite.js
+++ js/cancersite.js
@@ -1,13 +1,30 @@
-function myFunction(num) {
-  document.getElementById("myDropdown" + num).classList.toggle("show");
-  }
+let closeDropdown = function() {
 
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(e) {
-  if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown" + num).classList.toggle("show");
-    if (myDropdown.classList.contains('show')) {
-      myDropdown.classList.remove('show');
+}
+function myFunction(num) {
+  closeDropdown();
+  let someClick = document.getElementById("myDropdown" + num);
+  if(someClick) {
+    someClick.classList.toggle("show");
+    closeDropdown = function() {
+      someClick.classList.remove("show");
+    }
+  } else {
+    closeDropdown = function() {      
     }
   }
+}
+
+// Closes the dropdown if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < myDropdown.length; i++) {
+    var openDropdown = myDropdown[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
 }
